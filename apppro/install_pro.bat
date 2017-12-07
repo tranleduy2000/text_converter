0b904c10e0ae72d324bfd78d4c995c1a757071ca
Duy <tranleduy1233@gmail.com>
2017-12-07 22:57:19 +0700

    Add screenshots

--- apppro/install_pro.bat
+++ apppro/install_pro.bat
@@ -2,4 +2,3 @@ cp build\outputs\apk\release\apppro-release.apk app-release.apk
 adb uninstall duy.com.text_converter.pro
 adb install -r  app-release.apk
 adb shell am start -n "com.duy.text_converter.pro/com.duy.text.converter.pro.MainActivityPro" -a android.intent.action.MAIN -c android.intent.category.LAUNCHER
-exit
