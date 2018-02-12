50fe34851b40dcc88faf1bbd563252d0cc3bd13c
Duy <tranleduy1233@gmail.com>
2018-02-12 17:58:39 +0700

    change padding

--- app/src/main/java/com/duy/text/converter/pro/fragment/CodecFileFragment.java
+++ app/src/main/java/com/duy/text/converter/pro/fragment/CodecFileFragment.java
@@ -79,7 +79,7 @@ public class CodecFileFragment extends Fragment implements View.OnClickListener 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_tool, container, false);
+        return inflater.inflate(R.layout.fragment_file, container, false);
     }
 
     @Override
